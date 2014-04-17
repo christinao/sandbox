af6ddf2779d4e4c14ac41f124df60692768fe56a
christinao <christina@littlevictorian.com>
2014-04-17 17:07:18 -0400

    initial project commit

---
+++ footer.php
@@ -0,0 +1,20 @@
+<?php
+/**
+* The footer template
+*
+* Contains the closing of <div id="main"> and all content after.
+*
+* @package Starter_Theme
+*/
+?>
+</div><!-- #main -->
+</div><!-- #page -->
+<footer id="colophon" role="contentinfo">
+<div id="copyright">
+&copy; <?php echo date("Y"); ?> <?php echo bloginfo( "name" ); ?><br>
+<a href="http://yoursite.com" rel="nofollow">theme by YOU!</a>
+</div>
+</footer><!-- #colophon -->
+<?php wp_footer(); ?>
+</body>
+</html>
